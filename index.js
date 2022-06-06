a235f74c8ca4b0c6bd31bd3194edfcfac123168f
abdul-dulal <dulal.dpi.387364@gmail.com>
2022-06-06 09:30:42 +0600

    get orders

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PROT || 4000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
@@ -17,6 +17,7 @@ async function run() {
   try {
     await client.connect();
     const mealCollection = client.db("redOnion").collection("meal");
+    const cartCollection = client.db("redOnion").collection("cart");
 
     app.get("/meal", async (req, res) => {
       const meal = await mealCollection.find().toArray();
@@ -24,8 +25,42 @@ async function run() {
     });
 
     app.get("/meal/:id", async (req, res) => {
-      const id = req.params._id;
-      const result = await mealCollection.findOne({ id });
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await mealCollection.findOne(query);
+      res.send(result);
+    });
+
+    // put cart
+    app.post("/addCart", async (req, res) => {
+      const cart = req.body;
+
+      const query = {
+        name: cart.name,
+        email: cart.email,
+      };
+      const exist = await cartCollection.findOne(query);
+      if (exist) {
+        return res.send({ success: false, addcart: exist });
+      }
+      const result = await cartCollection.insertOne(cart);
+      res.send({ success: true, result });
+    });
+
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    // delete order
+    app.delete("/deleteorder/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await cartCollection.deleteOne(filter);
       res.send(result);
     });
   } finally {
