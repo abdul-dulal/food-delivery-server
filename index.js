c2e79e03bc297483104e379b01febe29350cf638
abdul-dulal <dulal.dpi.387364@gmail.com>
2022-06-24 10:14:41 +0600

    payment-method-add

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 4000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
@@ -22,6 +24,22 @@ async function run() {
     const lunchCollection = client.db("redOnion").collection("lunch");
     const dinnerCollection = client.db("redOnion").collection("dinner");
 
+    //payment intenet
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const service = req.body;
+
+      const price = service.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+
     app.get("/meal", async (req, res) => {
       const meal = await mealCollection.find().toArray();
       res.send(meal);
@@ -35,17 +53,11 @@ async function run() {
       res.send(dinner);
     });
 
-    app.get("/meal/:id", async (req, res) => {
+    app.get("/breakfast/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await mealCollection.findOne(query);
 
-      res.send(result);
-    });
-    app.get("/dinner/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await dinnerCollection.findOne(query);
       res.send(result);
     });
     app.get("/lunch/:id", async (req, res) => {
@@ -53,6 +65,12 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const result = await lunchCollection.findOne(query);
 
+      res.send(result);
+    });
+    app.get("/dinner/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await dinnerCollection.findOne(query);
       res.send(result);
     });
 
@@ -87,6 +105,26 @@ async function run() {
       const filter = { _id: ObjectId(id) };
       const result = await cartCollection.deleteOne(filter);
       res.send(result);
+    });
+    //payment
+    app.get("/breakfist-payment/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const paymentResult = await mealCollection.findOne(filter);
+      res.send(paymentResult);
+    });
+    app.get("/lunch-payment/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const filter = { _id: ObjectId(id) };
+      const paymentResult = await lunchCollection.findOne(filter);
+      res.send(paymentResult);
+    });
+    app.get("/dinner-payment/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const paymentResult = await dinnerCollection.findOne(filter);
+      res.send(paymentResult);
     });
   } finally {
   }
